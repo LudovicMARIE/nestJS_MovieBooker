d1f0b4385a2ed771927ca8586a3343211f666254
Ludovic MARIE <ludovic.marie@aol.com>
2025-02-07 15:56:27 +0100

    fix ?!

--- frontend/movie_booker/src/components/Movies.js
+++ frontend/movie_booker/src/components/Movies.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import MovieReservation from "./MovieReservation";
 
 const Movies = ({ token }) => {
